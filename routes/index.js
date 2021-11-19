9e7f8f7f0e16a47e1624ae9d0b8cdcbccacf3672
Arge Niño <arge.nino@uptc.edu.co>
2021-11-19 15:21:00 -0500

    Add pages, and menu responsive

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,15 @@ const router = Router();
 
 
 router.get('/', (req, res) => {
-
     res.render('index');
+});
 
+router.get('/register-user', (req, res)=>{
+    res.render('register-user');
+});
+
+router.get('/register-production', (req, res)=>{
+    res.render('register-production');
 });
 
 module.exports = router;
